20f2cd5ccd6fe40572fb4b6adb9c5dd275875805
jukha <34809502+jukha@users.noreply.github.com>
2023-09-21 11:54:04 +0500

    Add resuable styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import { useState } from "react";
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  border: solid;
+`;
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1>Hi</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <h2>Hello</h2>
+          <h3>World</h3>
+        </Row>
+        <Row type="horizontal">
+          <h2>ABC</h2>
+          <h3>XYZ</h3>
+        </Row>
+        <h1>Hello</h1>
+        <Heading>I am heading</Heading>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -47,3 +47,18 @@ const variations = {
     }
   `,
 };
+
+const Button = styled.button`
+  border: none;
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+  ${(props) => sizes[props]}
+  ${(props) => variations[props.variations]}
+`;
+
+Button.defaultProps = {
+  variation: "primary",
+  size: "medium",
+};
+
+export default Button;
